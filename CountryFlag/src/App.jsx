e480435d2ea038dbfc280a7f867b828b7174d338
Sujwalnaik <sujwalbn.a0@gmail.com>
2024-07-02 22:11:28 +0530

    created country flag

---
+++ CountryFlag/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CountryCard from "./component/CountryCard";
+
+function App() {
+  return (
+    <>
+      Country flag
+      <CountryCard />
+    </>
+  );
+}
+
+export default App;
